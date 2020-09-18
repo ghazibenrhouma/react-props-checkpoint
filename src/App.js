41b7610bef91b966722b1f6f465002761b37d7d8
ghazibenrhouma <ghazybenrhouma@gmail.com>
2020-09-18 12:24:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Profile from './profile/Profile';
 
 function App() {
+  const imgStyle = {
+    width: 300,
+    height: 350,
+    borderRadius: "15px 80px"
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Profile firstName = "Drake" bio="Drake , de son vrai nom Aubrey Drake Graham, est un rappeur-compositeur, chanteur et acteur canadien, né le 24 octobre 1986 à Toronto, au Canada. Longtemps affilié au label de Lil Wayne Young Money Entertainment, il y a signé officiellement en juin 2009. Son premier album studio, Thank Me Later, sort le 14 juin 2010. " profession="Singer">
+      <div>
+      <img src="/drake.jpg" alt="Tom Hardy Smiles" style={imgStyle}/>
+      </div>
+    </Profile>
+    <Profile firstName = "Lionel Messi" bio="Lionel Andrés Messi Cuccittini is an Argentine professional footballer who plays as a forward and captains both Spanish club Barcelona and the Argentina national team." profession="Football Player">
+    <div>
+    <img src="/messi.jpg" alt="Lionel Messi with Argentina kit" style={imgStyle}/>
     </div>
+  </Profile>
+  </>
   );
 }
 
